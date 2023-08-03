12e6fb83c9c06ed1fb29ff5e450a6f1a08f6e713
Dario Renzulli <dario.renzulli@gmail.com>
2023-08-03 13:01:41 -0300

    add environment agnostic join

--- ping.py
+++ ping.py
@@ -3,26 +3,28 @@ import subprocess
 
 def ping(denodo_home_path: str, trust_path: str, hostname: str, timeout: str, vdbname: str, usr: str, passwd: str):
     lib_path_vdp = os.path.join(denodo_home_path, "lib")
-    denodo_classpath = os.path.join(lib_path_vdp, "vdp-client-core", "denodo-vdp-client.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "denodo-asyncrmi.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "vdp-client-core", "denodo-vdp-tool.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "commons-util.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "commons-codec.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "commons-collections4.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "commons-lang3.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "commons-pool2.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "icu4j.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "log4j-api.jar")
-    denodo_classpath += ":" + os.path.join(lib_path_vdp, "contrib", "log4j-core.jar")
+    denodo_classpath = os.pathsep.join([
+        os.path.join(lib_path_vdp, "vdp-client-core", "denodo-vdp-client.jar"),
+        os.path.join(lib_path_vdp, "contrib", "denodo-asyncrmi.jar"),
+        os.path.join(lib_path_vdp, "vdp-client-core", "denodo-vdp-tool.jar"),
+        os.path.join(lib_path_vdp, "contrib", "commons-util.jar"),
+        os.path.join(lib_path_vdp, "contrib", "commons-codec.jar"),
+        os.path.join(lib_path_vdp, "contrib", "commons-collections4.jar"),
+        os.path.join(lib_path_vdp, "contrib", "commons-lang3.jar"),
+        os.path.join(lib_path_vdp, "contrib", "commons-pool2.jar"),
+        os.path.join(lib_path_vdp, "contrib", "icu4j.jar"),
+        os.path.join(lib_path_vdp, "contrib", "log4j-api.jar"),
+        os.path.join(lib_path_vdp, "contrib", "log4j-core.jar")
+    ])
     
-    conf_path_vdp = os.path.join(denodo_home_path, "conf")
+    conf_path_vdp = os.path.join(config.denodo_home_path, "conf")
     dbtools_path = os.path.join(conf_path_vdp, "db-tools")
     if os.path.exists(conf_path_vdp) and not os.path.exists(dbtools_path):
-        denodo_classpath += ":" + conf_path_vdp
+        denodo_classpath = os.pathsep.join([denodo_classpath, conf_path_vdp])
     elif os.path.exists(dbtools_path):
-        denodo_classpath += ":" + dbtools_path
+        denodo_classpath = denodo_classpath = os.pathsep.join([denodo_classpath, dbtools_path])
     else:
-        denodo_classpath += ":" + dbtools_path
+        denodo_classpath = os.pathsep.join([denodo_classpath, dbtools_path])
  
     java_opts = "-Djavax.net.ssl.trustStore=" + trust_path
     ping_command = f"java {java_opts} -classpath {denodo_classpath} com.denodo.vdb.vdbinterface.client.tools.Ping -t {timeout} \
